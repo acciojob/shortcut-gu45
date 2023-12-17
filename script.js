3908750ac13d7b0e7b356e73ba985708415c09cf
gu45 <86247735+gu45@users.noreply.github.com>
2023-12-18 00:41:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function shortcut(s1, s2) {
   // your code here
+	  let res = "";
+  let trim = str.split(" ");
+  res = trim[0][0]
+
+  trim = str1.split(" ")
+  res = res+trim[0][0]
+
+// can not handle space....what is the logic for handeling space
+	
+  return res
 }
 
 // Do not change the code below.
